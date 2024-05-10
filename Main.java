7090c5d6cb433dc2225b1559ded421a4550d25fd
WhityWolf <felipehac@hotmail.com>
2024-05-09 23:45:40 -0300

    correcao #13

--- Main.java
+++ Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes", "Sair"));
-        String selectedOption = mainMenu.getSelection();
+        int selectedOption = mainMenu.getSelection();
         
         System.out.println(selectedOption + " foi selecionada");
 
-        if (selectedOption.equals("Sair")) {
+        if (selectedOption == 4) {
             System.out.println("Encerrando a aplicação...");
             System.exit(0);
         }
